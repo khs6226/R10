3dd35703e8b0edcb4de585841f9db7cedc80bbf3
Hongsoon Kim <hkim@Hongsoons-MBP.hitronhub.home>
2020-01-15 17:45:37 -0800

    delete unnecessary code

--- index.js
+++ index.js
@@ -5,23 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './js/App';
 import {name as appName} from './app.json';
-import ApolloClient from 'apollo-boost';
-import { gql } from 'apollo-boost';
-
-const client = new ApolloClient({
-    uri: 'https://r10.academy.red/graphql',
-})
-
-client.query({
-    query: gql`
-        {
-            allConducts {
-                id
-                title
-                order
-            }
-        }
-    `
-}).then(result => console.log(result));
 
 AppRegistry.registerComponent(appName, () => App);
